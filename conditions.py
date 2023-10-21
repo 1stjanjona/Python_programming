8386ce68095bd5c682e45cc0aa173ad96d31d0d7
1stjanjona <jannat.hammouche@outlook.com>
2023-10-21 16:00:09 +0100

    done

--- conditions.py
+++ conditions.py
@@ -1,12 +1,11 @@
+#!/usr/bin/python3
 a = 5
 b = 8
 if a == b:
     print("a is equal to b")
-elif a != b:
-    print("a isn't equal to b")
-elif a > b:
-    print("a is bigger than b")
-elif a < b:
-    print("a is lesser than b")
+elif a != b and a > b:
+    print("a isn't equal to b, a is bigger than b")
+elif a != b and a < b:
+    print("a isn't equal to b, a is lesser than b")
 else:
     print("Error")
